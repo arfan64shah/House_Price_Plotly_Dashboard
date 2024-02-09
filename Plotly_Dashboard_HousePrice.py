63e8b16c70f2eca741ebe744ec7040ce30778bd3
arfan64shah <arfan64shah@gmail.com>
2024-02-09 14:47:27 +0500

    Added changes

--- Plotly_Dashboard_HousePrice.py
+++ Plotly_Dashboard_HousePrice.py
@@ -14,33 +14,44 @@ app = dash.Dash()
 
 # build a layout
 app.layout = html.Div([
-    html.H1("Dushanbe House Price Analysis"),
-    html.Div("Dashboard contains Price Analysis for Houses in Dushanbe"),
-    
-    dcc.Graph(
-        id = 'scatter-graph',
-        figure = {
-            'data': [
-                go.Scatter(x = dataset['number_of_rooms'], y = dataset['price'], marker = dict(color = 'green'), name = 'No of Rooms VS Price')
+    html.H1("Dushanbe House Price Analysis", style={'textAlign': 'center', 'backgroundColor': '#f8c471', 
+                                                    'paddingTop': '10px', 'marginTop':'0px', 'width':'100%', 
+                                                    'height':'60px'}),
+    #html.Div("Dashboard contains Price Analysis for Houses in Dushanbe"),
+    html.Div([
+        dcc.Graph(
+            id = 'scatter-graph',
+            figure = {
+                               
+                'data': [                    
+                    go.Scatter(x = dataset['number_of_rooms'], y = dataset['price'], marker = dict(color = 'green'), name = 'No of Rooms VS Price')
             ],
-            'layout': go.Layout(
-                title = 'Rooms VS Price',
-                xaxis = {'title' : 'No of Rooms'},
-                yaxis = {'title' : 'Price'}
+                'layout': go.Layout(
+                    title = 'Rooms VS Price',
+                    xaxis = {'title' : 'No of Rooms'},
+                    yaxis = {'title' : 'Price'}
+            )   }
             )
-            
-        }
-    ),
+    ],
+        style={'width': '49%', 'display': 'inline-block'}
+        ),
     # plot a bar graph
-    dcc.Graph(
-        id = 'bar-graph',
-        figure = {
-            'data': [
-                go.Line(x = dataset['number_of_rooms'], y = dataset['price'])
-            ]
-        }
-    )
-    
+    html.Div([
+        dcc.Graph(
+            id = 'histogram',
+            figure = {
+                'data': [
+                    go.Histogram(x = dataset['number_of_rooms'], nbinsx = 6)
+                ],
+                'layout': go.Layout(
+                    title = "Number of Rooms",
+                    bargap = 0.2
+                )
+            }
+        )
+    ],
+             style = {'width': '49%', 'display' : 'inline_block'}
+             )
 ])
 
 # start the app and server
